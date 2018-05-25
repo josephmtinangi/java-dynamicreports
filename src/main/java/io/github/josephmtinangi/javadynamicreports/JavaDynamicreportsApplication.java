5d51322b2e2f35d2607fb3c472adba1efdace029
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-25 13:52:44 +0300

    Initial commit

---
+++ src/main/java/io/github/josephmtinangi/javadynamicreports/JavaDynamicreportsApplication.java
@@ -0,0 +1,12 @@
+package io.github.josephmtinangi.javadynamicreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDynamicreportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDynamicreportsApplication.class, args);
+	}
+}
---
+++ src/main/java/io/github/josephmtinangi/javadynamicreports/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.github.josephmtinangi.javadynamicreports;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JavaDynamicreportsApplication.class);
+	}
+
+}
